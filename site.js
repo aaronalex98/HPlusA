b2a7b3548269413cb76b49d0ddeb12199a8a7015
aaronalex98 <aaronalex1998@gmail.com>
2020-12-03 16:59:33 +0530

    1. Header for all three viewport sizes 2. Hamburger icon animation for the menu icon in mobile view

---
+++ site.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+
+  $('.menu-toggle').click(function() {
+
+    $('.menu').toggleClass('menu-open', 300);
+    $(this).toggleClass('menu-toggle-open');
+
+  });
+
+
+
+
+});
